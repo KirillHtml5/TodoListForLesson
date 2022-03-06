1f7ea669362a240e78fc041dbf5473b1a7587dfa
KirillHtml5 <mr.kiril17@icloud.com>
2022-03-06 11:46:18 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,116 @@
+import React, {useState} from 'react';
+import './App.css';
+import {TaskType, Todolist} from './Todolist';
+import {v1} from 'uuid';
+
+export type FilterValuesType = "all" | "active" | "completed";
+type  TodoListsType = {
+    id: string
+    title: string
+    filter: FilterValuesType
+}
+type TasksObjectType = {
+    [key: string]: Array<TaskType>
+}
+
+function App() {
+    let todolistID1 = v1();
+    let todolistID2 = v1();
+
+    let [todolists, setTodolists] = useState<Array<TodoListsType>>([
+        {id: todolistID1, title: 'What to learn', filter: 'all'},
+        {id: todolistID2, title: 'What to buy', filter: 'all'},
+    ])
+
+    let [tasks, setTasks] = useState<TasksObjectType>({
+        [todolistID1]: [
+            {id: v1(), title: "HTML&CSS", isDone: true},
+            {id: v1(), title: "JS", isDone: true},
+            {id: v1(), title: "ReactJS", isDone: false},
+            {id: v1(), title: "Rest API", isDone: false},
+            {id: v1(), title: "GraphQL", isDone: false},
+        ],
+        [todolistID2]: [
+            {id: v1(), title: "HTML&CSS2", isDone: true},
+            {id: v1(), title: "JS2", isDone: true},
+            {id: v1(), title: "ReactJS2", isDone: false},
+            {id: v1(), title: "Rest API2", isDone: false},
+            {id: v1(), title: "GraphQL2", isDone: false},
+        ]
+    });
+
+
+    function removeTask(id: string, todolistID: string) {
+        // let currentTaskArray = tasks[todolistID]
+        // tasks[todolistID] = currentTaskArray.filter(t => t.id !== id);
+        // setTasks({...tasks});
+        setTasks({...tasks, [todolistID]:tasks[todolistID].filter(el=> el.id!==id)})
+    }
+
+    function addTasks(title: string, todolistID: string) {
+        let newTask = {id: v1(), title: title, isDone: false};
+        // let newTasks = [task, ...tasks];
+         setTasks({...tasks, [todolistID]:[newTask,...tasks[todolistID]]});
+    }
+
+    function changeStatus(taskId: string, isDone: boolean, todolistID: string) {
+        setTasks({...tasks, [todolistID]:tasks[todolistID].map(el=>el.id===taskId ? {...el,isDone:isDone}: el )})
+        // let task = tasks.find(t => t.id === taskId);
+        // if (task) {
+        //     task.isDone = isDone;
+        // }
+        //
+        // setTasks([...tasks]);
+    }
+
+
+    // let tasksForTodolist = tasks;
+    //
+    // if (filter === "active") {
+    //     tasksForTodolist = tasks.filter(t => t.isDone === false);
+    // }
+    // if (filter === "completed") {
+    //     tasksForTodolist = tasks.filter(t => t.isDone === true);
+    // }
+
+    function changeFilter(value: FilterValuesType, todolistID: string) {
+        // setFilter(value);
+        setTodolists(todolists.map(el => el.id===todolistID ? {...el, filter:value}: el))
+    }
+
+ const removeTodolist=(todolistID: string)=>{
+        setTodolists(todolists.filter(el=> el.id !== todolistID))
+     delete tasks[todolistID]
+ }
+    return (
+        <div className="App">
+            {todolists.map(el => {
+                let tasksForTodolist = tasks[el.id];
+
+                if (el.filter === "active") {
+                    tasksForTodolist = tasks[el.id].filter(t => t.isDone === false);
+                }
+                if (el.filter === "completed") {
+                    tasksForTodolist = tasks[el.id].filter(t => t.isDone === true);
+                }
+                return (
+                    <Todolist
+                        key={el.id}
+                        todolistID={el.id}
+                        title={el.title}
+                        tasks={tasksForTodolist}
+                        removeTask={removeTask}
+                        changeFilter={changeFilter}
+                        addTasks={addTasks}
+                        changeTaskStatus={changeStatus}
+                        filter={el.filter}
+                        removeTodolist={removeTodolist}
+                    />
+                )
+            })}
+
+        </div>
+    );
+}
+
+export default App;
